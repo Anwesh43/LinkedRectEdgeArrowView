b3b1443bd541d6c9e8ae6201330666f9c646fd1c
Anwesh43 <anweshthecool0@gmail.com>
2020-10-15 02:18:46 +0530

    adding RAENode class

--- rectedgearrowview/src/main/java/com/anwesh/uiprojects/rectedgearrowview/RectArrowEdgeView.kt
+++ rectedgearrowview/src/main/java/com/anwesh/uiprojects/rectedgearrowview/RectArrowEdgeView.kt
@@ -143,4 +143,45 @@ class RectArrowEdgeView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RAENode(var i : Int, val state : State = State()) {
+
+        private var next : RAENode? = null
+        private var prev : RAENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RAENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRAENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RAENode {
+            var curr : RAENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
