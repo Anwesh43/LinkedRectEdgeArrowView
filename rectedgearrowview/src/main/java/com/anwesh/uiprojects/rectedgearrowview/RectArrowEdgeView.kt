b418667803e152d64d85e8225295d2bbee623acc
Anwesh43 <anweshthecool0@gmail.com>
2020-10-15 02:11:53 +0530

    adding RectArrowEdgeView class

--- rectedgearrowview/src/main/java/com/anwesh/uiprojects/rectedgearrowview/RectArrowEdgeView.kt
+++ rectedgearrowview/src/main/java/com/anwesh/uiprojects/rectedgearrowview/RectArrowEdgeView.kt
@@ -80,3 +80,19 @@ fun Canvas.drawRAENode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawRectArrowEdge(scale, w, h, paint)
 }
+
+class RectArrowEdgeView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
