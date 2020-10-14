60518b3a2ac2af28cbf7e00ae1b2dc2a67a07a52
Anwesh43 <anweshthecool0@gmail.com>
2020-10-15 01:55:19 +0530

    adding cosntants and libraries

--- rectedgearrowview/src/main/java/com/anwesh/uiprojects/rectedgearrowview/RectArrowEdgeView.kt
+++ rectedgearrowview/src/main/java/com/anwesh/uiprojects/rectedgearrowview/RectArrowEdgeView.kt
@@ -3,3 +3,29 @@ package com.anwesh.uiprojects.rectedgearrowview
 /**
  * Created by anweshmishra on 15/10/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Path
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#4CAF50",
+        "#2196F3",
+        "#FF9800",
+        "#673AB7"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val barWFactor : Float = 6.9f
+val barHFactor : Float = 12.3f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
