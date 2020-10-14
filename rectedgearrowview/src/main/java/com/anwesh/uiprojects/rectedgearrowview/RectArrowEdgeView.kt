784851cc088704a49c43246b8b1ef30f2c2d1292
Anwesh43 <anweshthecool0@gmail.com>
2020-10-15 02:21:42 +0530

    adding RectArrowEdge class

--- rectedgearrowview/src/main/java/com/anwesh/uiprojects/rectedgearrowview/RectArrowEdgeView.kt
+++ rectedgearrowview/src/main/java/com/anwesh/uiprojects/rectedgearrowview/RectArrowEdgeView.kt
@@ -184,4 +184,27 @@ class RectArrowEdgeView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class RectArrowEdge(var i : Int) {
+
+        private var curr : RAENode = RAENode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUdpating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
