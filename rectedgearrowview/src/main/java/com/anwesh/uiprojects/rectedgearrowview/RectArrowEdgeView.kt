bfb16506add646dae5f6b9f27da9ff9e90be03f5
Anwesh43 <anweshthecool0@gmail.com>
2020-10-15 02:10:41 +0530

    adding Canvas extension methods

--- rectedgearrowview/src/main/java/com/anwesh/uiprojects/rectedgearrowview/RectArrowEdgeView.kt
+++ rectedgearrowview/src/main/java/com/anwesh/uiprojects/rectedgearrowview/RectArrowEdgeView.kt
@@ -12,6 +12,8 @@ import android.graphics.Paint
 import android.graphics.Color
 import android.graphics.Path
 import android.graphics.Canvas
+import android.graphics.RectF
+
 
 val colors : Array<Int> = arrayOf(
         "#F44336",
@@ -34,3 +36,47 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawRectArrowEdge(scale : Float, w : Float, h : Float, paint : Paint) {
+    val hBar : Float = h / barHFactor
+    val wBar : Float = w / barWFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        for (k in 0..1) {
+            save()
+            translate(0f, -hBar / 2 + hBar * j)
+            drawLine(0f, 0f, wBar * 0.5f * sf1, 0f, paint)
+            drawLine(wBar * 0.5f, 0f, wBar * 0.5f + hBar * 0.5f * sf2, (1f - 2 * k) * hBar * 0.5f * sf2, paint)
+            restore()
+        }
+        save()
+        val path : Path = Path()
+        path.moveTo(0f, -hBar / 2)
+        path.lineTo(wBar / 2, -hBar / 2)
+        path.lineTo(wBar / 2 + hBar / 2, 0f)
+        path.lineTo(wBar / 2, hBar / 2)
+        path.lineTo(0f, hBar / 2)
+        path.lineTo(0f, -hBar / 2)
+        clipPath(path)
+        drawRect(RectF(0f, -hBar / 2, (wBar + hBar / 2) * sf3, hBar / 2), paint)
+        restore()
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawRAENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRectArrowEdge(scale, w, h, paint)
+}
