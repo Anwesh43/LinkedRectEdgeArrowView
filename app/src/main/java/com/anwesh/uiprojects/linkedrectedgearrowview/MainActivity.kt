e13cbc58f6e14dabdcfc592ab22c060efbe20c4d
Anwesh43 <anweshthecool0@gmail.com>
2020-10-15 02:29:36 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedrectedgearrowview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrectedgearrowview/MainActivity.kt
@@ -2,11 +2,18 @@ package com.anwesh.uiprojects.linkedrectedgearrowview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.WindowManager
+import com.anwesh.uiprojects.rectedgearrowview.RectArrowEdgeView
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RectArrowEdgeView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
